dd9cda5074214b6a9eff1c98524f5418e1dbbba5
SzaYbuS <szaybus@gmail.com>
2018-05-12 11:33:50 +0200

    wyświetlanie w tabeli

--- index.php
+++ index.php
@@ -6,11 +6,13 @@ $q = "SELECT * FROM ksiazka";
 //$q = "DELETE FROM ksiazka WHERE id=3";
 $wynik = $db->query($q);
 //while($wiersz = $wynik->fetch_row()){
+echo "<table border=1>";
 while($wiersz = $wynik->fetch_assoc()){
+  echo "<tr>";
   foreach ($wiersz as $kolumna => $wartosc) {
-    echo $kolumna . " = ". $wartosc . " ";
+    echo "<td>" . $kolumna . "</td><td>". $wartosc . "</td>";
   }
-  echo "<br>";
+  echo "</tr>";
 }
-
+echo "</table>";
 ?>
